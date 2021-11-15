d9d253800f11d85484e739c2ac4999660d03bbde
firailanufika <92341932+firailanufika@users.noreply.github.com>
2021-11-15 17:28:32 +0700

    Add files via upload

---
+++ Praktikum7.java
@@ -0,0 +1,24 @@
+package praktikum7;
+
+
+import java.util.Scanner;
+
+public class Praktikum7 {
+    public static void main (String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        while(true) {
+            System.out.println("Masuk ke Sistem Parkir? y/n");
+            char masuk = in.nextLine().charAt(0);
+
+            if (masuk == 'y') {
+                new Programy();
+            }
+            if (masuk == 'n') {
+                break;
+            }
+
+        }
+    }
+}
